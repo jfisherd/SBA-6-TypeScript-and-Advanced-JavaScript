3f7874edd0fdcf9c19906875ed2d8abd95cd4b2b
Josh Fisher <jfisherd@gmail.com>
2025-11-24 01:55:11 -0500

    docs: apiService export fix

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,43 +2,44 @@ import { Product } from "../models/Product.js"
 import { NetworkError } from "../utils/errorHandler.js"
 import { DataError } from "../utils/errorHandler.js"
 
-let fetchProductData = async (): Promise<Product[]|Error> => {
-    try {
-        let myProductArray: Product[] = []
+export const contactApi = () => {
+    let fetchProductData = async (): Promise<Product[] | Error> => {
+        try {
+            let productArray: Product[] = []
 
-        let myResponse = await fetch('https://dummyjson.com/products')
+            let response = await fetch('https://dummyjson.com/products')
 
-        if (!myResponse.ok) {
-            throw new NetworkError('Failed to receive a response from the API')
+            if (!response.ok) {
+                throw new NetworkError('Failed to receive a response from the API')
+            }
+
+            let data = await response.json()
+            let objectArray = data.products
+
+            for (let i = 0; i < objectArray.length; i++) {
+                productArray[i] = new Product(
+                    objectArray[i].id,
+                    objectArray[i].title,
+                    objectArray[i].description,
+                    objectArray[i].category,
+                    objectArray[i].price,
+                    objectArray[i].discountPercentage,
+                    objectArray[i].sku
+                )
+            }
+
+            return productArray
         }
-
-        let data = await myResponse.json()
-        let myObjectArray = data.products
-
-        for (let i = 0; i < myObjectArray.length; i++) {
-            myProductArray[i] = new Product(
-                myObjectArray[i].id,
-                myObjectArray[i].title,
-                myObjectArray[i].description,
-                myObjectArray[i].category,
-                myObjectArray[i].price,
-                myObjectArray[i].discountPercentage,
-                myObjectArray[i].sku
-            )
+        catch (error) {
+            if (error instanceof DataError) {
+                console.error('A data error has occurred: ', error)
+            } else if (error instanceof NetworkError) {
+                console.error('A data error has occurred: ', error)
+            } else {
+                console.error('An unexpected has occurred: ', error)
+            }
+            return new Error('error')
         }
-
-        return myProductArray
-    }
-    catch (error) {
-        if (error instanceof DataError) {
-            console.error('A data error has occurred: ', error)
-        } else if (error instanceof NetworkError) {
-            console.error('A data error has occurred: ', error)
-        } else {
-            console.error('An unexpected has occurred: ', error)
-        }
-        return new Error('error')
     }
 }
-
 
